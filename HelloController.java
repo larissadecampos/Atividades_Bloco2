176d61a2421daf82f27854fd7d4fe00ee65cd84e
larissadecampos2@gmail.com <larissadecampos2@gmail.com>
2022-02-17 18:39:29 -0300

    Spring Boot_Atividades 1 e 2

---
+++ HelloController.java
@@ -0,0 +1,21 @@
+package com.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hello")
+
+public class HelloController {
+	
+	@GetMapping
+	 public String hello() {
+		
+		
+		return "As habilidades são persistência e mentalidade de crescimento.</br>"
+				+ "Essa semana pretendo aprender sem sofrer. "; 
+		
+	}
+	
+}
